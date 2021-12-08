57a5a73a8b4b138dd51cd2ba116ca8e8f7deee32
Mil-Mourao <miltonmourao@gmail.com>
2021-12-07 21:27:52 -0300

    Subiendo el trabajo conjunto inicial.

--- Website/src/app.js
+++ Website/src/app.js
@@ -2,12 +2,12 @@ const path = require('path')
 const express = require ("express")
 const app = express()
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', 3000)
 const publicPath = path.resolve(__dirname, "../public")
 app.use(express.static(publicPath))
 
 app.listen(app.get('port'),() => console.log('running on port http://localhost:' + app.get('port')))
 
-app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")))
+app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname,"views","index.html")))
 app.get("/register", (req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")))
 app.get("/login", (req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")))
