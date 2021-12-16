0bc7b23851e0cd0119c2383cd0026baef1d70a27
brab0210 <bernardoarrechea@gmail.com>
2021-12-16 13:16:02 -0300

    ajustes finales

--- Website/src/app.js
+++ Website/src/app.js
@@ -11,5 +11,5 @@ app.listen(app.get('port'),() => console.log('running on port http://localhost:'
 app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname,"views","index.html")))
 app.get("/register", (req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")))
 app.get("/login", (req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")))
-app.get("/carrito", (req,res)=> res.sendFile(path.resolve(__dirname,"views","carrito.html")))
-app.get("/product", (req,res)=> res.sendFile(path.resolve(__dirname,"views","product.html")))
+app.get("/carrito", (req,res)=> res.sendFile(path.resolve(__dirname,"views","productCart.html")))
+app.get("/product", (req,res)=> res.sendFile(path.resolve(__dirname,"views","productDetail.html")))
