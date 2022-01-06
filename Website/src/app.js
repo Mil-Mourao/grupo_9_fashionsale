eb8350391c8d49423a197f6ada3d5ec5c8abffa8
brab0210 <bernardoarrechea@gmail.com>
2022-01-06 14:29:41 -0300

    se agrega ejs y se cambia los html

--- Website/src/app.js
+++ Website/src/app.js
@@ -1,19 +1,19 @@
 const path = require('path')
 const express = require ("express")
 const app = express()
+const method = require('method-override')
 
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 const publicPath = path.resolve(__dirname, "../public")
 app.use(express.static(publicPath))
+app.use(express.urlencoded({extended: true}));
+app.use(method('m')); // ?m=PUT || ?m=DELETE
 
 app.listen(app.get('port'),() => console.log('running on port http://localhost:' + app.get('port')))
 
 app.use(require('./routes/main'))
-
 app.use('/users', require('./routes/users'))
-
 app.use('/products',require('./routes/products'))
-
-app.get("/", (req,res)=> res.sendFile(path.resolve(__dirname,"views","index.html")))
-app.get("/users", (req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")))
-app.get("/product", (req,res)=> res.sendFile(path.resolve(__dirname,"views","productDetail.html")))
