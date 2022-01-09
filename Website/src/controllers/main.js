277af5ffe18e4737277c8d70c03cace3bd7b1594
brab0210 <bernardoarrechea@gmail.com>
2022-01-08 22:46:35 -0300

    vistas dinamicas finalizadas

--- Website/src/controllers/main.js
+++ Website/src/controllers/main.js
@@ -1,8 +1,8 @@
 
 const controller = {
-    index: (req,res) => res.render('index', {css:'index', title: "Home"}),
+    index: (req,res) => res.render('index', {styles:['index','general','header','footer'], title: "Home"}),
     about: (req,res) => res.render('about', {css:'about'}),
-    carrito: (req,res) => res.render('productCart', {css:'carrito'}),
+    cart: (req,res) => res.render('productCart', {styles:['carrito','general','header','footer'], title: "Mi carrito"}),
 }
 module.exports = controller
 
