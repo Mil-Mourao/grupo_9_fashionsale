9e78133e470c2097285161bef8f50fcd3dfccade
Bernardo Arrechea Böndsted <74820674+brab0210@users.noreply.github.com>
2022-01-10 18:20:07 -0300

    Revert "Bernardo"

--- Website/src/controllers/users.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const controller = {
-    login: (req,res) => res.render('users/login', {styles:['login'], title: "Log in"}),
-    register: (req,res) => res.render('users/register', {styles:['register'], title: "Registro"})
-    }
-
-module.exports = controller
