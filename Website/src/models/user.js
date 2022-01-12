47777f9c45d498538e8f8e8b7c0bf825e3f93cfc
Mil-Mourao <miltonmourao@gmail.com>
2022-01-12 19:53:01 -0300

    agregando carpeta models, product.js, user.js. actualizando users.JSON

---
+++ Website/src/models/user.js
@@ -0,0 +1,14 @@
+[
+  {
+    "name": "One",
+    "lastName": "Two",
+    "email": "OneTwo@onetwo.com",
+    "password":"passwrod1"
+  },
+  {
+    "name": "Three",
+    "lastName": "Four",
+    "email": "ThreeFour@onetwo.com",
+    "password":"T1me2F1sh"
+  },
+];
