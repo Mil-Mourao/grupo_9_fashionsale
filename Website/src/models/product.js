4b9147813a3be93a605748499478e5a2e7b6675d
NicolasCid <nicolass.cid@gmail.com>
2022-01-17 17:34:19 -0300

    Create.css

--- Website/src/models/product.js
+++ Website/src/models/product.js
@@ -7,8 +7,8 @@ model = {
         id: model.all().length == 0 ? 1 : model.all().pop().id +1,
         name: data.name,
         price: parseInt(data.precio),
-        offert: data.offert ? true : false,
-        imagen: data.files.map (f => file.create(f).id)}),
+        offert: data.offert ? true : false}),
+        /*imagen: data.files.map (f => file.create(f).id)}),*/
     read: () => fs.readFileSync(model.file, 'utf8'),
     write: data => fs.writeFileSync(model.file, JSON.stringify(data,null,2)),
     all: () => JSON.parse(model.read()),
