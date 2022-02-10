705179dc019e38bd0a91f97c0c4ddff71614c916
Mil-Mourao <miltonmourao@gmail.com>
2022-02-09 21:21:41 -0300

    actualicé el css de update, el formulario de modify ahora no sobreescribe los campos vacíos

--- Website/src/models/product.js
+++ Website/src/models/product.js
@@ -1,55 +1,70 @@
-const fs = require('fs');
-const path = require('path');
-const file = require('../models/file');
+const fs = require("fs");
+const path = require("path");
+const file = require("../models/file");
 
 const model = {
-    file: path.resolve(__dirname, '../data/products.json'),
-    read: () => fs.readFileSync(model.file, 'utf8'),
-    write: data => fs.writeFileSync(model.file, JSON.stringify(data,null,2)),
-    all: () => JSON.parse(model.read()),
-    generate: data => Object({
-        id: model.all().length == 0 ? 1 : model.all().pop().id +1,
-        name: data.name,
-        size: data.size,
-        description: data.description,
-        price: parseFloat(data.price),
-        category: data.category,
-        ofert: data.ofert ? true : false,
-        discount: data.discount,
-        img: data.file.length > 0 ? data.file.map(f => file.create(f).id) : null
-        }),
-    create: data => {
-        let newProduct = model.generate(data);
-        let all = model.all();
-        all.push(newProduct);
-        model.write(all);
-        return newProduct
-    },    
-    delete: id => {
-        model.search('id', id).img.map(e => file.delete(e))
-        model.write(model.all().filter(e => e.id != id))
-    },
-    search: (field, value) => model.all().find(element => element[field] == value),
-    update: (id,data) => {
-        let all = model.all();
-        let updated = all.map(e =>{
-            if(e.id == id) {
-                e.name = data.name,
-                e.price = parseInt(data.price),
-                e.size = data.size,
-                e.description = data.description,
-                e.category = data.category,
-                e.ofert = data.ofert ? true : false,
-                e.category = data.category,
-                e.discount = data.discount,
-                e.img = data.file && data.file.length > 0 ? data.file.map(f => file.create(f).id) : null
-            return e
-            }
-            return e   
-        })
-        model.write(updated)
-        let product = model.search('id',id);
-        return product
-    },
-}
-module.exports = model
+  file: path.resolve(__dirname, "../data/products.json"),
+  read: () => fs.readFileSync(model.file, "utf8"),
+  write: (data) => fs.writeFileSync(model.file, JSON.stringify(data, null, 2)),
+  all: () => JSON.parse(model.read()),
+  generate: (data) =>
+    Object({
+      id: model.all().length == 0 ? 1 : model.all().pop().id + 1,
+      name: data.name,
+      size: data.size,
+      description: data.description,
+      price: parseFloat(data.price),
+      category: data.category,
+      ofert: data.ofert ? true : false,
+      discount: data.discount,
+      img:
+        data.file.length > 0 ? data.file.map((f) => file.create(f).id) : null,
+    }),
+  create: (data) => {
+    let newProduct = model.generate(data);
+    let all = model.all();
+    all.push(newProduct);
+    model.write(all);
+    return newProduct;
+  },
+  delete: (id) => {
+    model.search("id", id).img.map((e) => file.delete(e));
+    model.write(model.all().filter((e) => e.id != id));
+  },
+  search: (field, value) => model.all().find(element => element[field] == value),
+//   search: (field, value) => {
+//     console.log("--------------------------",field, value);
+//     model.all().find((element) => element[field] == value);
+//     let all = model.all();
+//     let found = all.find((element) => element[field] == value);
+//     value.name = found.name;
+//     console.log(found)
+//   },
+  update: (id, data) => {
+    let all = model.all();
+    let updated = all.map((e) => {
+      if (e.id == id) {
+          (e.name != "") ? data.name : e.name,
+          (e.price != "") ? parseInt(data.price) : e.price,
+          (e.size != "") ? data.size : e.size,
+        //   e.size = data.size,
+          (e.description != "") ? data.description : e.description,
+          (e.category != "") ? data.category : e.category,
+          data.ofert ? true : false,
+        //   e.ofert = data.ofert ? true : false,
+          (e.category != "") ? data.category : e.category,
+          (e.discount != "") ? data.discount : e.discount,
+          e.img =
+            data.file && data.file.length > 0
+              ? data.file.map((f) => file.create(f).id)
+              : null;
+        return e;
+      }
+      return e;
+    });
+    model.write(updated);
+    let product = model.search("id", id);
+    return product;
+  },
+};
+module.exports = model;
