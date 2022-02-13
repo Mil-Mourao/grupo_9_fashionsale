ceb3e891e3d57b629b00c93d182853dee3041eb5
brab0210 <bernardoarrechea@gmail.com>
2022-02-13 15:58:53 -0300

    arreglo de msg de error del register

--- Website/src/middlewares/save.js
+++ Website/src/middlewares/save.js
@@ -5,7 +5,7 @@ const validations = [
         let search = user.search('email', value);
         return search ? Promise.reject('Ese email ya se encuentra en nuestro sistema.') : Promise.resolve();
     }),
-    validator.body('password').isLength({min: 6}).withMessage('Mínimo 6 caracteres').matches(/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@"]).*$/).withMessage("Mínimo 6 caracteres, 1 letra Mayúscula, 1 número, 1 caracter especial por ejemplo (!#$%&?@)")
+    validator.body('password').isLength({min: 6}).withMessage('Mínimo 6 caracteres').matches(/^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@"]).*$/).withMessage("Mínimo debe contener 6 caracteres, <br/>1 letra Mayúscula, <br/>1 número, <br/>1 caracter especial por ejemplo (!#$%&?@).")
 ]
 
 module.exports = validations;
