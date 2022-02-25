487a3f2984404e1678b4916d32a16a4d7f2c05ac
brab0210 <bernardoarrechea@gmail.com>
2022-02-25 00:21:28 -0300

    branch para backup 100% funcional con json

--- Website/public/js/burgerMenu.js
+++ Website/public/js/burgerMenu.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector('.hamburgesa');
 const toggle_close= document.querySelector('.hamburgesa_close');
 const menuToggle = document.querySelector('.navbar-content');
 
-    toggle.addEventListener('click', () => menuToggle.classList.toggle('active'))
+    toggle.addEventListener('click', () => menuToggle.classList.add('active'))
     toggle_close.addEventListener('click', () => menuToggle.classList.remove('active'))
 
 
