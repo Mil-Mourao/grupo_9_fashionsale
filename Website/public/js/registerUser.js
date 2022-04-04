7780c41f9816c0ce67bff705ea2a4b374a03fdbe
brab0210 <bernardoarrechea@gmail.com>
2022-04-03 23:51:50 -0300

    up

--- Website/public/js/registerUser.js
+++ Website/public/js/registerUser.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", function () {
     let formulario = document.querySelector("#form")
-    let button = document.querySelector(".button")
 
     const regex = /^.*(?=.{6,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@"]).*$/
     const regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/i
